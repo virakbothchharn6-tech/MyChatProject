17807aa95fd09804825d1fea1f16a59b7707b030
virakbothchham6-tech <virakbothchharn6 GitHub>
2026-07-18 10:58:49 +0700

    feat: complete session 2.2 - implement form requests and api resources

--- laravel-app/app/Http/Requests/User/SignupRequest.php
+++ laravel-app/app/Http/Requests/User/SignupRequest.php
@@ -1,4 +1,4 @@
-<?php
+﻿<?php
 
 namespace App\Http\Requests\User;
 
@@ -7,26 +7,17 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class SignupRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
     public function authorize(): bool
     {
         return true;
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, ValidationRule|array<mixed>|string>
-     */
     public function rules(): array
     {
         return [
             'name' => 'required|string|max:100',
             'email' => 'required|email|unique:users,email',
-            'password' => 'required|string|min:6|max:10|confirmed',
-            'callback_url' => 'required|url',
+            'password' => 'required|string|min:6|max:10|confirmed'
         ];
     }
 }
